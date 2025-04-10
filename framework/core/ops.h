b071849acca535b39e1e48beaeb030c117a8ca97
Haifeng Jin <haifeng-jin@users.noreply.github.com>
2025-04-10 16:10:33 -0700

    ops.h

--- framework/core/ops.h
+++ framework/core/ops.h
@@ -5,31 +5,32 @@
 
 namespace ops {
 // Forward operations
-void matmul(const Tensor &a, const Tensor &b, Tensor &output);
-void add_row_broadcast(const Tensor& a, const Tensor& b, Tensor &output);
-void add_element_wise_(Tensor &a, const Tensor &b);
-void multiply(const Tensor &a, const Tensor &b, Tensor &output);
-void relu(const Tensor& tensor, Tensor &output);
-void softmax(const Tensor& tensor, Tensor &output);
-void log(const Tensor& tensor, Tensor &output);
-void sum(const Tensor& tensor, Tensor &output);
+void matmul(const Tensor &x, const Tensor &y, Tensor &output);
+void add_row_broadcast(const Tensor& x, const Tensor& y, Tensor &output);
+void add_element_wise_(Tensor &x, const Tensor &y);
+void multiply(const Tensor &x, const Tensor &y, Tensor &output);
+void relu(const Tensor& x, Tensor &output);
+void softmax(const Tensor& x, Tensor &output);
+void log(const Tensor& x, Tensor &output);
+void sum(const Tensor &x, Tensor &output);
 
 // Backward operations
-void matmul_backward(const Tensor &output_grad, const Tensor &a, const Tensor &b, 
-                     Tensor &a_grad, Tensor &b_grad);
-void add_row_broadcast_backward(const Tensor &output_grad, const Tensor &a, const Tensor &b,
-                  Tensor &a_grad, Tensor &b_grad);
-void multiply_backward(const Tensor &output_grad, const Tensor &a, const Tensor &b,
-                       Tensor &a_grad, Tensor &b_grad);
-void relu_backward(const Tensor &output_grad, const Tensor &input,
-                   Tensor &input_grad);
-void softmax_backward(const Tensor &output_grad, const Tensor &input_data,
-                      Tensor &input_grad);
-void log_backward(const Tensor &output_grad, const Tensor &input,
-                  Tensor &input_grad);
-void sum_backward(const Tensor &output_grad, const Tensor &tensor, 
-                  Tensor &input_grad);
+void matmul_backward(const Tensor &output_grad, const Tensor &x, const Tensor &y,
+                      Tensor &x_grad, Tensor &y_grad);
+void add_row_broadcast_backward(const Tensor &output_grad, const Tensor &x, const Tensor &y,
+                         Tensor &x_grad, Tensor &y_grad);
+void multiply_backward(const Tensor &output_grad, const Tensor &x, const Tensor &y,
+                          Tensor &x_grad, Tensor &y_grad);
+void relu_backward(const Tensor &output_grad, const Tensor &x,
+                         Tensor &x_grad);
+void softmax_backward(const Tensor &output_grad, const Tensor &x,
+                          Tensor &x_grad);
+void log_backward(const Tensor &output_grad, const Tensor &x,
+                        Tensor &x_grad);
+void sum_backward(const Tensor &output_grad, const Tensor &x,
+                        Tensor &x_grad);
 
 };
 
 #endif // OPS_H
+
