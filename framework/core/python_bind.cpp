1ea95252f9deaf9d9a30babbf38aff18ab540c72
Haifeng Jin <haifeng-jin@users.noreply.github.com>
2025-04-10 15:40:50 -0700

    docs for pybind

--- framework/core/python_bind.cpp
+++ framework/core/python_bind.cpp
@@ -3,31 +3,57 @@
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
 
-PYBIND11_MODULE(core, m) { // the module name matches the package name.
+/**
+ * @brief Bind C++ functions & classes to Python.
+ *
+ * This function is how pybind11 binds the C++ implementation of the functions
+ * and classes to Python functions and classes in a Python module.
+ *
+ * All the bindings are created by calling the .def() member function.
+ * The parameters to .def() is usually:
+ *   * String. The name of the function in Python.
+ *   * A pointer to the function.
+ *   * Docstring for the function in Python.
+ *
+ * An exception is when binding the constructors of a class, we only specify
+ * the parameter types. Pybind11 will find the constructor on its own.
+ *
+ * @param core: The name of the Python module.
+ * @param m: The module object to bind classes and functions to.
+ */
+PYBIND11_MODULE(core, m) {
+    // Bind C++ Tensor class to framework.core.Tensor with all of its member
+    // functions.
     py::class_<Tensor>(m, "Tensor")
+        // For shape only constructor.
         .def(py::init<const std::vector<size_t> &>())
+        // For shape and data constructor.
         .def(
             py::init<const std::vector<size_t> &, const std::vector<float> &>())
+        // For the copy_to_numpy() member function.
         .def("copy_to_numpy", &Tensor::copy_to_numpy);
 
-    auto ops_module = m.def_submodule("ops"); // ops submodule is still present.
+    // Create a submodule named ops under framework.core.
+    auto ops_module = m.def_submodule("ops");
 
-    // Forward operations
-    ops_module.def("matmul", &ops::matmul,
-                   "Matrix multiplication of two 2-d tensors.");
+    // Bind all the forward operations under the framework.core.ops.
+    // For example, framework.core.ops.matmul().
+    ops_module.def(
+        "matmul",                  // Function name in Python.
+        &ops::matmul,              // Pointer to the function.
+        "(m, k), (k, n) -> (m, n)" // Docstring of the function in Python.
+    );
     ops_module.def("add_row_broadcast", &ops::add_row_broadcast,
-                   "Add (1, n) to (m, n), broadcast by row.");
+                   "(m, n), (1, n) -> (m, n)");
     ops_module.def("add_element_wise_", &ops::add_element_wise_,
-                   "Add (m, n) to (m, n) element-wise in-place.");
-    ops_module.def("multiply", &ops::multiply,
-                   "Element-wise multiply for a tensor.");
-    ops_module.def("relu", &ops::relu, "The relu op for tensors.");
-    ops_module.def("softmax", &ops::softmax,
-                   "The softmax op row-wise for a 2d tensor.");
-    ops_module.def("log", &ops::log, "The element-wise log op for tensors.");
-    ops_module.def("sum", &ops::sum, "The sum all the elements in a tensor.");
+                   "(m, n), (m, n)");
+    ops_module.def("multiply", &ops::multiply, "(m, n), (m, n) -> (m, n)");
+    ops_module.def("relu", &ops::relu, "(m, n) -> (m, n)");
+    ops_module.def("softmax", &ops::softmax, "(m, n) -> (m, n)");
+    ops_module.def("log", &ops::log, "(m, n) -> (m, n)");
+    ops_module.def("sum", &ops::sum, "(m, n) -> (1,)");
 
-    // Backward operations
+    // Bind all the backward operations under the framework.core.ops.
     ops_module.def("matmul_backward", &ops::matmul_backward,
                    "Backward pass for matrix multiplication.");
     ops_module.def("add_backward", &ops::add_row_broadcast_backward,
